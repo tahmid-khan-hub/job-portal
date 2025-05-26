ffe11fc520ef10342bca2d839af880015db07469
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-27 01:09:30 +0600

    added contexts, firebase, updated register

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import {
   RouterProvider,
 } from "react-router";
 import router from './Router/Router.jsx';
+import AuthProvider from './contexts/AuthProvider.jsx';
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,6 +3,19 @@ import React from "react";
 import registerLottie from '../../assets/lotties/register.json'
 
 const Register = () => {
+
+    const handleRegister = e =>{
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        console.log(email, password);
+
+        
+
+    }
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -14,14 +27,12 @@ const Register = () => {
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
             <h1 className="text-3xl font-bold">Register now!</h1>
-            <form className="fieldset">
+            <form onSubmit={handleRegister} className="fieldset">
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input type="email" name="email" className="input" placeholder="Email" />
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
+              <input type="password" name="password" className="input" placeholder="Password" />
+              
               <button className="btn btn-neutral mt-4">Register</button>
             </form>
           </div>
