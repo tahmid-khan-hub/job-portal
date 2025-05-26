a9ac73fd57ccfeb348619f649504fddd58aab757
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-27 00:00:35 +0600

    build structure

---
+++ src/Router/Router.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {
+  createBrowserRouter,
+} from "react-router";
+import RootLayout from '../layouts/RootLayout';
+import Home from '../pages/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children:[
+        {
+            index:true,
+            Component: Home,
+        }
+    ]
+  },
+]);
+
+export default router;
