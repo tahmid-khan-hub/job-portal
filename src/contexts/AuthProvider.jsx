6be536cd6957db4191c85db97d515bf7c7e5d28a
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-31 18:31:32 +0600

    working on cookies related things

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
 import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { app } from '../firebase/firebase.init';
+import axios from 'axios';
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -37,6 +38,20 @@ const AuthProvider = ({children}) => {
         const unSubscribe = onAuthStateChanged(auth, currentUser =>{
             setUser(currentUser);
             setLoading(false);
+            if(currentUser?.email){
+                const userData = {email: currentUser.email};
+                axios.post('http://localhost:3000/jwt', userData, {
+                    withCredentials: true // for cookies
+                })
+                    .then(res => {
+                        console.log('token after jwt', res.data);
+
+                        // simple way to store token but not recommended is - store in local storage
+                        // const token = res.data.token;
+                        // localStorage.setItem('token', token);
+                    })
+                    .catch(err => console.log(err))
+            }
         })
         return () =>{
             unSubscribe();
