bd48e929d6c30d8e8c9c2c35468a94d4d08b75dc
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-06-01 00:39:53 +0600

    added credentials in fetch for myApplicationPromise

--- src/api/applicationsApi.js
+++ src/api/applicationsApi.js
@@ -1,3 +1,5 @@
 export const myApplicationsPromise = email =>{
-    return fetch(`http://localhost:3000/applications?email=${email}`).then(res => res.json())
+    return fetch(`http://localhost:3000/applications?email=${email}`, {
+        credentials: 'include'
+    }).then(res => res.json())
 }
